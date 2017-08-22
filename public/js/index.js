6316f18348e82c2547f0a591c6dd8ad056d735e0
Jeff <jeffschultz.js@gmail.com>
2017-08-21 17:00:03 -0700

    fixed nodemailer, still working on it

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 'use strict';
-//nodemailer
-const nodemailer = require('nodemailer');
+
 
 //doc ready
 $(document).ready(function(){
@@ -35,44 +34,26 @@ $('.tablinks').on('click', function(){
 
 //send email w/ nodemailer
 $('#sendBtn').on('click', function(event){
-    var senderAddress = $('#email').val().trim();
-    console.log(senderAddress);
-    var title = $("#title").val();
-    console.log(title);
-    var message = $('#message').val();
-    console.log(message);
+    var from, subject, text;
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // secure:true for port 465, secure:false for port 587
-        auth: {
-            user: 'jsonlineresume@gmail.com',
-            pass: 'Asimplepass4me!'
-        }
+
+    $("#send_email").click(function(){      
+        var from = $('#email').val().trim();
+        console.log(from);
+        var subject = $("#title").val();
+        console.log(subject);
+        var text = $('#message').val();
+        console.log(text);
+        
+
+        $.get("https://jeffschultzresume.herokuapp.com/send",{from:from, subject:subject, text:text}, function(data){
+            if (data =="sent") {
+                $('#email').val("");
+                $('#title').val("");
+                $('#message').val("");
+                $('#myModal').modal('toggle');
+                alert('Your email has been sent, thank you!');
+            }
+        });
     });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: senderAddress, // sender address
-        to: 'jeffschultz.js@gmail.com', // list of receivers
-        subject: title, // Subject line
-        text: message, // plain text body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
-    }); 
-
-    $('#email').val('');
-    $('#title').val('');
-    $('#message').val('');
-
-    $('#myModal').modal('toggle');
-    return false;
 });
--- server.js
+++ server.js
@@ -1,14 +1,50 @@
 var express = require("express");
 var path = require("path");
+//nodemailer
+const nodemailer = require('nodemailer');
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+var smtpTransport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: "jsonlineresume@gmail.com",
+        pass: "Apass4you!"
+    }
+});
+
+
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get('/send', function(req, res) {
+
+  var mailOptions={
+    to : "jeffschultz.js@gmail.com",
+    from: req.query.from,
+    subject : req.query.subject,
+    text : req.query.text
+  }
+
+  console.log(mailOptions);
+
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if (error){
+      console.log(error);
+      res.end("error");
+    }
+    else {
+      console.log("Message sent");
+      res.end("sent");
+    }
+  });
 });
 
 app.listen(PORT, function(){
