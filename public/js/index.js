e70e78d034574e6d2c52410a95ff00d386e06f00
Jeff <jeffschultz.js@gmail.com>
2017-09-01 22:28:28 -0700

    making my online resume great again

--- public/js/index.js
+++ public/js/index.js
@@ -1,64 +1,14 @@
-'use strict';
-
-
-//doc ready
-$(document).ready(function(){
-    //$('.card').hide();
-    $('.defaultOpen').fadeIn(2500);
-    $('img').hide();
-    //$('.card').slideDown(2500);
-    $('img').fadeIn(3000);
+$('#navbarIcon').on('click', function(e){
+    if($('#navWrap').hasClass('navInactive')){
+        $('#navWrap').removeClass('navInactive');
+        $('#navWrap').addClass('navActive');
+        $('#navbarIcon').removeClass('fa-caret-right');
+        $('#navbarIcon').addClass('fa-caret-left');
+    } else {
+        $('#navWrap').removeClass('navActive');
+        $('#navWrap').addClass('navInactive');
+        $('#navbarIcon').removeClass('fa-caret-left');
+        $('#navbarIcon').addClass('fa-caret-right');
+    }
 });
 
-//set active class when clicked on to display hidden info
-$('.tablinks').on('click', function(){
-    var link = $(this).attr('id');
-    $('.tablinks').each(function(){
-        if ($(this).attr('id') !== link){
-            $(this).removeClass('active');
-        } else {
-            $(this).addClass('active');
-        }
-    });
-
-    $('.tab-pane').each(function(){
-        if ($(this).attr('id') !== link){
-            $(this).hide();
-        } else {
-            //$(this).show();
-            $(this).addClass("animated fadeInDown");
-            $(this).show();
-        }
-    });
-});
-
-//send email w/ nodemailer
-$('#sendBtn').on('click', function(event){
-    var from, subject, text;
-      
-        from = $('#email').val().trim();
-        //console.log(from);
-        subject = $("#title").val();
-        //console.log(subject);
-        text = $('#message').val();
-        //console.log(text);
-
-        text += "       ||  Sender: " + from;
-        
-        $('#emailMessage').html("Your email is sending, please wait...");
-
-        $.get("https://jeffschultzresume.herokuapp.com/send", {from:from, subject:subject, text:text})
-        .done(function(data) {
-            console.log(data);
-            $('#email').val("");
-            $('#title').val("");
-            $('#message').val("");
-            $('#emailMessage').html("Your email has been sent. Thank you!");
-            setTimeout(function(){
-                $('#myModal').modal('toggle');
-                $('#emailMessage').html("");
-            }, 2000);
-            
-        });
-        
-});
